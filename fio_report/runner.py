fec2f6b3a56d11779d2ccb0f0caae76db0cb2162
tao.xu <t2k8>
2022-12-08 16:21:21 +0800

    +example

--- fio_report/runner.py
+++ fio_report/runner.py
@@ -16,14 +16,16 @@ from fio_report.report_xlsx import ReportXlsx
 
 class FIOReportRunner(object):
     """FIO测试结果收集，报告生成"""
-    def __init__(self, data_path, *args, **kwargs):
+    def __init__(self, data_path, output, *args, **kwargs):
         self.data_path = data_path
+        self.output = output
 
     def run(self):
         json_data = FIOJsonParse(self.data_path).get_json_data()
         # with open("./sss.json", "w+") as f:
         #     f.write(json.dumps(json_data, indent=2))
-        ReportXlsx("./", json_data).create_xlsx_file()
+        ReportXlsx(self.output, json_data).create_xlsx_file()
+        logger.log("DESC", "输出结果：{}".format(self.output))
 
 
 if __name__ == '__main__':
