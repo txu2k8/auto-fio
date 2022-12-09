be1b0329ceec437541d82bed24043efcf54d6094
tao.xu <t2k8>
2022-12-09 16:06:07 +0800

    fix bw

--- fio_report/runner.py
+++ fio_report/runner.py
@@ -15,15 +15,16 @@ from fio_report.report_xlsx import ReportXlsx
 
 class FIOReportRunner(object):
     """FIO测试结果收集，报告生成"""
-    def __init__(self, data_path, output, *args, **kwargs):
+    def __init__(self, data_path, output, comments, *args, **kwargs):
         self.data_path = data_path
         self.output = output
+        self.comments = comments
 
     def run(self):
         json_data = FIOJsonParse(self.data_path).get_json_data()
         # with open("./sss.json", "w+") as f:
         #     f.write(json.dumps(json_data, indent=2))
-        ReportXlsx(self.output, json_data).create_xlsx_file()
+        ReportXlsx(self.output, json_data, comments=self.comments).create_xlsx_file()
         logger.log("DESC", "输出结果：{}".format(self.output))
 
 
